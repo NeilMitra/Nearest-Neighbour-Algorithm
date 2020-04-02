ac050f237359f06a08fa3677aa1dde8a71c9dacb
NeilMitra <mitra.neil@gmail.com>
2020-04-02 09:56:42 -0400

    Creating third coordinate imput
    Creating the third coordinate input - still a very ineffective and inefficient algorithm - more fluidity is required possibly using recursive functions.

--- nearestneighbour.py
+++ nearestneighbour.py
@@ -5,7 +5,8 @@ import math as m
 
 def distanceformula():
     print(m.sqrt( ((y2-y1)**2) + ((x2-x1)**2)))
-    
+
+
 
 print("Enter your two starting coordinates:")
 
@@ -39,6 +40,18 @@ y1 = (sp[1])
 x2 = (sc[0])
 y2 = (sc[1])
 
+print("What is your third coordinate?:")
+
+tc = []
+
+for i in range(0, 2): 
+    x = int(input()) 
+  
+    tc.append(x)       
+print(tc)
+
+xa2 = (tc[0])
+ya2 = (tc[1])
 
 distanceformula()
 
